281ade6b279ed30dce6ba83806e97fa9f306700e
Kayli <kayliryan@gmail.com>
2022-07-28 11:10:52 -0700

    Hat List delete working. Need to fix formatting next

--- ghi/app/src/HatList.js
+++ ghi/app/src/HatList.js
@@ -2,8 +2,32 @@ import React from "react";
 
 import { NavLink } from 'react-router-dom';
 
-function HatList(props) {
-    return (
+
+class HatList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {hats: []}
+    this.deleteHat = this.deleteHat.bind(this);
+  }
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:8090/api/hats/');
+    if (response.ok) {
+      const data = await response.json();
+      this.setState({hats: data.hats});
+    }
+  }
+
+  async deleteHat(hat) {
+    await fetch(`http://localhost:8090/api/hats/${hat.id}/`, { method: 'DELETE' });
+    let index = this.state.hats.indexOf(hat);
+    let updated_hats = [...this.state.hats];
+    updated_hats.splice(index,1)
+    this.setState({hats: updated_hats})
+  }
+
+render() {
+  return (
     <React.Fragment>
     <div className="container-fluid">
         <NavLink className="navbar-brand" to="/hats/create">Create a Hat</NavLink>
@@ -18,19 +42,16 @@ function HatList(props) {
           </tr>
         </thead>
         <tbody>
-          {/* for (let attendee of props.attendees) {
-            <tr>
-              <td>{ attendee.name }</td>
-              <td>{ attendee.conference }</td>
-            </tr>
-          } */}
-          {props.hats.map(hat => {
+          {this.state.hats.map(hat => {
             return (
               <tr key={hat.id}>
                 <td>{ hat.style }</td>
                 <td>{ hat.fabric }</td>
                 <td>{ hat.color }</td>
+                {hat.picture_url &&
                 <td><img src={ hat.picture_url } style={{height:"100px", width:"100px"}}></img></td>
+                }
+                <td><button onClick={() => this.deleteHat(hat)}>Delete</button></td>
               </tr>
             );
           })}
@@ -39,5 +60,7 @@ function HatList(props) {
       </React.Fragment>
     )
 }
+}
+
 
 export default HatList
